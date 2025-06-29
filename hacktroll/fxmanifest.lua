48e5f2daaf6574458da61f43957bb66dd9017690
Ryzo <anonuser5171@gmail.com>
2025-06-29 22:52:43 +0300

    Add files via upload
    a "hack" player by (id)

---
+++ hacktroll/fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Ryzo'
+description 'Fake hacking effect with sound and visuals'
+version '1.0'
+
+dependency 'es_extended'  -- <- הוסף שורה זו
+
+client_script 'client.lua'
+
+server_script 'server.lua'
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/script.js',
+    'html/sounds/hack.ogg'
+}
